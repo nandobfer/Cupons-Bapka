d58ebe299900200b2fbaf90df00ba8cb56fb835a
nandobfer <nandobfer@yahoo.com.br>
2022-04-12 01:00:43 -0300

    Reorganized config.py

--- data/config.py
+++ data/config.py
@@ -1,3 +1,15 @@
+# quantidade de cupons
+N_CUPONS = 1
+
+# clients data file
+CLIENTS_DATA = 'data/clients.json'
+
+# data constants
+CUPONS = "Cupons"
+NOME = "Nome"
+CPF = "CPF"
+EMAIL = "E-mail"
+
 # Requests definitions
 def requestClientId():
     return int(input('Digite o ID de cliente: '))
@@ -10,16 +22,3 @@ def requestSignUp():
 SIGNUP = 's'
 
 # Database definitions
-
-
-# quantidade de cupons
-N_CUPONS = 1
-
-# clients data file
-CLIENTS_DATA = 'data/clients.json'
-
-# data constants
-CUPONS = "Cupons"
-NOME = "Nome"
-CPF = "CPF"
-EMAIL = "E-mail"
